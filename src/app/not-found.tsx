3b33457b505da137b662276840a723ff738a38c2
Joao Pereira <joaopereira2213@gmail.com>
2024-09-28 11:45:17 +0100

    style: 404 page static vinyl img

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,50 +4,33 @@ import Image from 'next/image';
 
 import Button from '@/app/components/common/Button/index';
 import { Tag } from '@/app/components/common/ImageTag/index';
-import Navbar from '@/app/components/layout/Navbar/index';
-
-const VinylPlayer = () => {
-    return (
-        <div className="text-foreground">
-            <div className="relative border-1 p-7">
-                <Image
-                    src="/vinyl-player.svg"
-                    alt="404 Not Found"
-                    width={575}
-                    height={500}
-                    className="p-4"
-                />
-                <Image
-                    src="/vinyl.svg"
-                    alt="Layered Vinyl"
-                    width={375}
-                    height={500}
-                    className="absolute left-1/2 top-1/2 ml-[-3.75rem] -translate-x-1/2 -translate-y-1/2 transform"
-                />
-                <Image
-                    src="/vinyl-design.svg"
-                    alt="Vinyl Design"
-                    width={125}
-                    height={150}
-                    className="absolute left-1/2 top-1/2 ml-[-3.75rem] -translate-x-1/2 -translate-y-1/2 transform"
-                />
-            </div>
-            <Tag tag="404.svg" hoverTag="Page Not Found" QRSrc="soft_dev" />
-        </div>
-    );
-};
 
 export default function NotFound() {
     return (
-        <div className="relative flex h-screen flex-col">
-            <Navbar />
-            <div className="flex items-center justify-center">
-                <div className="w-[650px] text-center">
-                    <VinylPlayer />
-                    <div className="font-HK mt-4 w-full grid-cols-2 text-3xs font-medium tracking-widest text-foreground sm:text-sm">
-                        <div className="text-right">
-                            <Button text="Return home" />
+        <div className="flex h-[100vh] xl:h-[120vh] items-center justify-center">
+            <div className="border border-foreground px-4 py-4 sm:px-24 sm:py-12 text-center">
+                <div className="text-foreground">
+                    <div className="flex justify-center">
+                        <div className="relative h-[20rem] w-[20rem] sm:h-[25rem] sm:w-[25rem] xl:h-[27.5rem] xl:w-[27.5rem] 2xl:h-[30rem] 2xl:w-[30rem]">
+                            <Image
+                                src="/complete-vinyl.svg"
+                                alt="404 Vinyl Setup"
+                                priority={true}
+                                fill
+                                sizes="(max-width: 1280px) 10rem, 26rem"
+                                style={{ objectFit: 'contain' }}
+                            />
                         </div>
+                    </div>
+                    <Tag
+                        tag="404.svg"
+                        hoverTag="Page Not Found"
+                        QRSrc="soft_dev"
+                    />
+                </div>
+                <div className="font-HK mt-4 w-full grid-cols-2 text-3xs font-medium tracking-widest text-foreground sm:text-sm">
+                    <div className="text-right">
+                        <Button text="Return home" href='/'/>
                     </div>
                 </div>
             </div>
