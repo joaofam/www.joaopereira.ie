01cf93e751fa88b47eff6627cb261e02dae83b43
Joao Pereira <joaopereira2213@gmail.com>
2024-10-06 00:48:39 +0100

    feat: draft for 404 page redesign

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,36 +2,18 @@
 
 import Image from 'next/image';
 
-import Button from '@/app/components/common/Button/index';
-import { Tag } from '@/app/components/common/ImageTag/index';
+import Scramble from './components/common/Scramble';
 
 export default function NotFound() {
     return (
-        <div className="flex h-[100vh] xl:h-[120vh] items-center justify-center">
-            <div className="border border-foreground px-4 py-4 sm:px-24 sm:py-12 text-center">
-                <div className="text-foreground">
-                    <div className="flex justify-center">
-                        <div className="relative h-[20rem] w-[20rem] sm:h-[25rem] sm:w-[25rem] xl:h-[27.5rem] xl:w-[27.5rem] 2xl:h-[30rem] 2xl:w-[30rem]">
-                            <Image
-                                src="/complete-vinyl.svg"
-                                alt="404 Vinyl Setup"
-                                priority={true}
-                                fill
-                                sizes="(max-width: 1280px) 10rem, 26rem"
-                                style={{ objectFit: 'contain' }}
-                            />
-                        </div>
-                    </div>
-                    <Tag
-                        tag="404.svg"
-                        hoverTag="Page Not Found"
-                        QRSrc="soft_dev"
-                    />
-                </div>
-                <div className="font-HK mt-4 w-full grid-cols-2 text-3xs font-medium tracking-widest text-foreground sm:text-sm">
-                    <div className="text-right">
-                        <Button text="Return home" href='/'/>
-                    </div>
+        <div className="flex h-screen items-center justify-center text-2xl text-foreground">
+            <div className="relative border p-8 border-foreground">
+            <span className="absolute text-xs left-4 top-0 -translate-y-1/2 transform bg-white px-2 font-bold lowercase text-[#3c4048] 2xl:text-2xs cursor-default before:absolute before:inset-0 before:bg-inherit before:z-[-1]">
+                        Page Not Found
+                    </span>
+                <div className="flex items-center uppercase">
+                    <p>Status Code: </p>
+                    <p>[<Scramble className='text-[#F33A21]'loop shouldScramble>404</Scramble>]</p>
                 </div>
             </div>
         </div>
