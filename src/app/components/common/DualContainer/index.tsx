81a5555c0228418a06cd622e09a05f7016e1061f
Joao Pereira <joaopereira2213@gmail.com>
2024-10-16 22:56:54 +0100

    style: dual container title converted to legend

--- src/app/components/common/DualContainer/index.tsx
+++ src/app/components/common/DualContainer/index.tsx
@@ -7,21 +7,27 @@ const DualContainer: React.FC<DualContainerProps> = ({
     rightTitle = 'Img',
 }) => {
     return (
-        <div className="flex h-full w-full flex-col items-center justify-center font-SpaceMono text-2xs text-foreground sm:text-xs 2xl:text-sm cursor-default">
-            <div className="grid-rows-auto sm:grid-rows-0 grid h-full w-full items-start gap-8 xl:grid-cols-2">
-                {/* Left Content Container (previously Right) */}
+        <div className="flex h-full w-full cursor-default flex-col items-center justify-center font-SpaceMono text-2xs text-foreground sm:text-xs 2xl:text-sm">
+            <div className="grid h-full w-full items-start gap-8 grid-rows-auto sm:grid-rows-0 xl:grid-cols-2">
+                {/* Left Content Container */}
                 <div className="relative flex h-full w-full items-center justify-center border border-foreground">
-                    <span className="absolute left-4 top-0 -translate-y-1/2 transform bg-white px-2 font-bold lowercase text-accent 2xl:text-sm cursor-default">
-                        {leftTitle}
+                    <span className="absolute left-4 top-0 -translate-y-1/2 transform cursor-default px-2 font-bold lowercase 2xl:text-sm">
+                        <span className="relative">
+                            <span className="absolute inset-0 bg-gradient-to-b from-white via-white to-transparent"></span>
+                            <span className="relative">{leftTitle}</span>
+                        </span>
                     </span>
                     {leftContent}
                 </div>
 
-                {/* Right Content Container (previously Left) */}
+                {/* Right Content Container */}
                 <div className="flex h-full w-full flex-col items-center justify-start sm:items-start">
                     <div className="relative h-full space-y-8 border border-foreground p-4 xl:p-10">
-                        <span className="absolute left-4 top-0 -translate-y-1/2 transform bg-white px-2 font-bold lowercase cursor-default">
-                            {rightTitle}
+                        <span className="absolute left-4 top-0 -translate-y-1/2 transform cursor-default px-2 font-bold lowercase">
+                            <span className="relative">
+                                <span className="absolute inset-0 bg-gradient-to-b from-white via-white to-transparent"></span>
+                                <span className="relative">{rightTitle}</span>
+                            </span>
                         </span>
                         {rightContent}
                     </div>
