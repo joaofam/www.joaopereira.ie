d5b22a823588f6cd583ff102991293c4cf3c8f32
Joao Pereira <joaopereira2213@gmail.com>
2024-10-28 20:54:05 +0000

    feat: header + favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -230,6 +230,26 @@ export default function RootLayout({
             className={`${SpaceMono.variable} ${Nohemi.variable} ${Scrawler.variable} ${ThrowUpFill.variable} ${ThrowUpHighlight.variable} ${ThrowUpShadow.variable} ${ThrowUpStroke.variable}`}
             suppressHydrationWarning={true}
         >
+            <head>
+                <link
+                    rel="icon"
+                    type="image/png"
+                    href="/favicon-96x96.png"
+                    sizes="96x96"
+                />
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <meta
+                    name="apple-mobile-web-app-title"
+                    content="Joao Pereira"
+                />
+                <link rel="manifest" href="/site.webmanifest" />
+            </head>
             <body
                 className={cn(
                     'min-h-screen bg-background font-SpaceMono antialiased'
